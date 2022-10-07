8f22df8783b118907afb0db4850b7cf5b6ecdb33
Ananthavijay <vj35.cool@gmail.com>
2022-10-07 12:05:52 +0530

    developed data scraper code

--- data-scraper/main.py
+++ data-scraper/main.py
@@ -1,16 +1,32 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import re
-
+import requests
+from fake_useragent import UserAgent
 
 def data_scraper(url, tag):
-    '''This function takes url and tag as parameters
-    and returns the scrapped data with respective to the url 
-    and later on we can probably play with the returned data'''
-    pass
-
+    res = []
+    ua = UserAgent()
+    headers = {'User-Agent':str(ua.random)}
+    try:
+        r = requests.get(url,headers=headers)
+    except requests.exceptions.RequestException as e:
+        return "\n Error: " + str(e)
+    soup = BeautifulSoup(r.text,'html.parser')
+    for data in soup.find_all(tag):
+        res.append(data.text)
+    return res
 
 if __name__ == "__main__":
-    url = input("Enter/Paste The Url")
-    tag = input('Enter the tag you wanna scrape')
+    url = input("[+] Enter/Paste The Url: ")
+    tag = input("[+] Enter the tag you wanna scrape: ")
     scrapped_data = data_scraper(url, tag)
+    if type(scrapped_data) is list:
+        if len(scrapped_data) == 0:
+            print("\n[-] Data not found with tag "+tag)
+        else:
+            print("\nScraped data: \n")
+            for data in scrapped_data:
+                print(data)
+    elif scrapped_data:
+        print(scrapped_data)
+
+        
